d5f1ec5b333ac68d074d62f53837c8bfc8a3a8db
Luis Manuel Castaño <mannuelcast321@gmail.com>
2023-10-18 06:11:57 -0500

    c.1.02

--- src/EntityComponentes/User/formQrPago.js
+++ src/EntityComponentes/User/formQrPago.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ImagenDeBanco from '../img-entityComponentes/imagen01hyjpg.jpg';
 
-
-function formQrPago() {
+function FormQrPago() {
     return (
         <div>
-            <img className="imgBanck" src={ImagenDeBanco} width={500} />
+            <img className="imgBanck" src={ImagenDeBanco} width={500} alt="Imagen del banco" />
         </div>
     )
 }
+
+export default FormQrPago;
