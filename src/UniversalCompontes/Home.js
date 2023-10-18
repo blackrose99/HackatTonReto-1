7c40150877a53315ed86cb0fa94bd3556fa7580f
manvepa <manuelparravelandia@gmail.com>
2023-10-17 23:17:18 -0500

    v.1.06

--- src/UniversalCompontes/Home.js
+++ src/UniversalCompontes/Home.js
@@ -3,16 +3,21 @@ import React from "react";
 import Footer from "./Footer";
 
 function Home() {
-  return (
-    <div>
-      <h1>Bienvenido a nuestra página de inicio</h1>
-      <p>Esta es una página de inicio de ejemplo.</p>
-      <button>¡Comenzar!</button>
-      <Footer/>
-    </div>
-  );
-  
-  
+    return (
+        <div>
+
+            <div className="diseñoHome">
+
+                <h1>Bienvenido a nuestra página de inicio</h1>
+                <p>Esta es una página de inicio de ejemplo.</p>
+                <button>¡Comenzar!</button>
+            </div>
+            <Footer />
+        </div>
+
+    );
+
+
 }
 
 export default Home;
