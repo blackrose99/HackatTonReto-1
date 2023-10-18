1162b5620dc6adf3a09b31cf7c05c65d9aa41131
Yeison0134 <yeisonduarte34@gmail.com>
2023-10-18 12:01:55 -0500

    v.1.15

--- src/UniversalCompontes/Footer.js
+++ src/UniversalCompontes/Footer.js
@@ -5,18 +5,47 @@ function Footer() {
   return (
     <footer>
       <div className="container-fluid">
+        <hr></hr>
         <div className="row">
           <div className="col-lg-3 datosContactos">
-          <i class='bx bx-user-circle'></i>
-          <div className="contact-info">
-            <p className="bold-text">CONTACT CENTER</p>
-            <p>01 8000 938 088</p>
-          </div>
-          <i class='bx bx-phone'></i>
-          <div className="contact-info">
-            <p className="bold-text">EN BUCARAMANGA</p>
-            <p>607 680 30 03</p>
-          </div>
+            <div className="contact-item">
+              <i class='bx bx-user-circle'></i>
+              <div className="contact-info">
+                <p className="bold-text">CONTACT CENTER</p>
+                <p className="right-text">01 8000 938 088</p>
+              </div>
+            </div>
+            <div className="contact-item">
+              <i class='bx bx-phone'></i>
+              <div className="contact-info">
+                <p className="bold-text">EN BUCARAMANGA</p>
+                <p className="right-text">607 680 30 03</p>
+              </div>
+            </div>
+            <div className="contact-item">
+              <i class='bx bxl-whatsapp'></i>
+              <div className="contact-info">
+                <p className="bold-text">WHATSAPP</p>
+                <p className="right-text">310 751 8024</p>
+              </div>
+            </div>
+            <div className="contact-item">
+              <i class='bx bx-list-check'></i>
+              <div className="contact-info">
+                <p className="bold-text">CONTINÚA CON LA</p>
+                <p className="right-text">firma de tus documentos</p>
+              </div>
+            </div>
+            <div className="contact-item">
+              <i class='bx bx-map' ></i>
+              <div className="contact-info">
+                <p className="bold-text">Edificio Administrativo</p>
+                <p className="right-text">Calle 35 # 16-43
+                Paseo del Comercio
+                NIT 804-009-752-8
+                Bucaramanga</p>
+              </div>
+            </div>
           </div>
           <div className="col-lg-3">
             <div className="footer-info">
