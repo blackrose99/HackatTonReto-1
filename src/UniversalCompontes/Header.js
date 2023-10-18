095d026cc48665b5bdbf9c446b18246b27249b92
luigistig <luigistig17@gmail.com>
2023-10-18 10:36:43 -0500

    E.1.05

--- src/UniversalCompontes/Header.js
+++ src/UniversalCompontes/Header.js
@@ -1,52 +1,49 @@
 import React from 'react';
-
-import "../UniversalCompontes/Header.css"
-
-
-
-
+import '../UniversalCompontes/Header.css';
+import ImagenDeLogoCoomultrasan from '../UniversalCompontes/ImgUniversal/logoCoomultrasan.png';
 
 const Header = ({ entityName, userName, userRole }) => {
   return (
     <div>
-    <header>
-    
-      <nav className="navbar navbar-expand-lg navbar-light bg-light" id='colorBarra'>
-        <div className="container">
-          
-          <a className="navbar-brand" href="/">Nombre de tu Cooperativa</a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/">Mis QRs</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/entidad">Nuevo Formulario</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/usuario">Panel de Usuario</a>
-              </li>
-            </ul>
-          </div>
-          <div className="user-info">
-            <p className="entity-name">Iniciar Sesión: {entityName}</p>
-            {/* <p className="user-name">Usuario: {userName}</p>
-            <p className="user-role">Rol: {userRole}</p> */}
-          </div>
+      <header >
+        <div className="header-container-fluid" id='colorTotal'>
+          <nav className="navbar navbar-expand-lg navbar-light container-fluid" id='colorBarra'>
+            <div className="container" id='colorBarra'>
+              <a className="navbar-brand" href="/" id='texto'>Nombre de tu Cooperativa</a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/" id='texto'>Mis QRs</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/entidad" id='texto'>Nuevo Formulario</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/usuario" id='texto'>Panel de Usuario</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="user-info">
+                <p className="entity-name" id='texto'>Iniciar Sesión: {entityName}</p>
+                {/* <p className="user-name">Usuario: {userName}</p>
+                <p className="user-role">Rol: {userRole}</p> */}
+              </div>
+            </div>
+          </nav>
+          <img src={ImagenDeLogoCoomultrasan} alt="Logo Coomultrasan" id='LogoImg' className="header-image" />
         </div>
-      </nav>
-    </header>
+      </header>
     </div>
   );
 };
--- src/UniversalCompontes/Home.js
+++ src/UniversalCompontes/Home.js
@@ -8,17 +8,10 @@ import "./Home.css";
 //Importar Footer 
 import Footer from "./Footer";
 
-import ImagenDeLogoCoomultrasan from '../UniversalCompontes/ImgUniversal/logoCoomultrasan.png';
 
 
 function Home() {
     return (
-<<<<<<< HEAD
-        <div class="container-fluid">
-            <div class="row">
-                <Header />
-            </div>
-=======
         <div>
             <div class="container-fluid">
                 <div class="row">
@@ -30,11 +23,10 @@ function Home() {
                 <div class="row">
                     <div class="col-lg-12">
                         <div className="diseñoHome">
-                            <img src={ImagenDeLogoCoomultrasan} alt="Logo Coomultrasan" ></img>
+                            
                             <h1>Bienvenido a nuestra página de inicio</h1>
                             <p>Esta es una página de inicio de ejemplo.</p>
                             <button>¡Comenzar!</button>
->>>>>>> 05f73d8c683fb82a9d415fe7093b3b804ab1d8cb
 
                         </div>
                     </div>
