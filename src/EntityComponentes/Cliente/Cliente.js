f04a8accdfce71de6547c6dd3db1f9658e73d8ac
Lewis Guerrero <lewisguerrero20@gmail.com>
2023-10-18 14:41:44 -0500

    l.1.2

--- src/EntityComponentes/Cliente/Cliente.js
+++ src/EntityComponentes/Cliente/Cliente.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 
 //Importaciones del Componente Header
@@ -26,7 +27,7 @@ const ClientInfo = () => {
       try {
         // Simular una solicitud a la API o la base de datos.
         // Reemplaza esta parte con tu lógica real.
-        const response = await fetch(`http://192.168.1.15:5000/api/users/${id}`);
+        const response = await fetch(`http://192.168.4.15:5000/api/users/${id}`);
         const data = await response.json();
         setClientData(data);
         setLoading(false);
