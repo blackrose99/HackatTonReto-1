c970cca696eaa00fa14df4f193cd10bea4a58b21
manvepa <manuelparravelandia@gmail.com>
2023-10-18 15:33:16 -0500

    p2:53

--- src/EntityComponentes/Cliente/Cliente.js
+++ src/EntityComponentes/Cliente/Cliente.js
@@ -1,36 +1,19 @@
 import React, { useState, useEffect } from 'react';
-
-//Importaciones del Componente Header
-import HeaderCliente from "./HeaderCliente";
-
-//Importaciones del Componente CuentaInfo
-
-
-import { Link, useNavigate, useParams } from "react-router-dom";
-import CuentaInfo from './cuentaInfo'
+import InfoClient from './infoclient'; // Importa el componente InfoClient (asegúrate de que la mayúscula inicial esté correcta)
+import { useParams } from "react-router-dom";
 
 const ClientInfo = () => {
   const { id } = useParams();
   const [clientData, setClientData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // Simular una solicitud para obtener la información del cliente por su ID
   useEffect(() => {
-    // Realiza una solicitud al servidor o utiliza datos en la base de datos
-    // para obtener la información del cliente por su ID (clientId).
-    // Supongamos que obtienes los datos y los almacenas en clientData.
-
-    // Simulación de datos para este ejemplo:
     const fetchData = async () => {
-      console.log(id)
       try {
-        // Simular una solicitud a la API o la base de datos.
-        // Reemplaza esta parte con tu lógica real.
         const response = await fetch(`http://192.168.4.15:5000/api/users/${id}`);
         const data = await response.json();
         setClientData(data);
         setLoading(false);
-        console.log(data)
       } catch (error) {
         console.error('Error al obtener datos del cliente', error);
         setLoading(false);
@@ -38,7 +21,7 @@ const ClientInfo = () => {
     };
 
     fetchData();
-  }, []);
+  }, [id]);
 
   if (loading) {
     return <p>Cargando la información del cliente...</p>;
@@ -50,15 +33,9 @@ const ClientInfo = () => {
 
   return (
     <>
-    <div>
-
-      <ClientInfo data={clientData}/>
-
+      <InfoClient data={clientData} /> {/* Pasa los datos a InfoClient */}
       {/* Mostrar más detalles según sea necesario */}
-
-    </div>
     </>
-    
   );
 };
 
