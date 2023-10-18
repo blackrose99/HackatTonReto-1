05f73d8c683fb82a9d415fe7093b3b804ab1d8cb
Yeison0134 <yeisonduarte34@gmail.com>
2023-10-18 09:00:23 -0500

    d.1.08

--- src/UniversalCompontes/Home.js
+++ src/UniversalCompontes/Home.js
@@ -8,20 +8,40 @@ import "./Home.css";
 //Importar Footer 
 import Footer from "./Footer";
 
-
+import ImagenDeLogoCoomultrasan from '../UniversalCompontes/ImgUniversal/logoCoomultrasan.png';
 
 
 function Home() {
     return (
         <div>
-            <Header />
-            <div className="diseñoHome">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <Header />
+                    </div>
+                </div>
 
-                <h1>Bienvenido a nuestra página de inicio</h1>
-                <p>Esta es una página de inicio de ejemplo.</p>
-                <button>¡Comenzar!</button>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div className="diseñoHome">
+                            <img src={ImagenDeLogoCoomultrasan} alt="Logo Coomultrasan" ></img>
+                            <h1>Bienvenido a nuestra página de inicio</h1>
+                            <p>Esta es una página de inicio de ejemplo.</p>
+                            <button>¡Comenzar!</button>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <Footer />
+                    </div>
+                </div>
+
             </div>
-            <Footer />
+            
+            
         </div>
 
     );
