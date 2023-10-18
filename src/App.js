2ace49f67f1dd46eb564e6fd13472db17f759c9c
Luis Manuel Castaño <mannuelcast321@gmail.com>
2023-10-17 21:47:39 -0500

    ver.01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Importar Home
+import Home from "./UniversalCompontes/Home";
+//Importar Footer 
+import Footer from "./UniversalCompontes/Footer";
+
+//Importar Formulario de pago con el codigo Qr
+import formQrPago from "./EntityComponentes/User/formQrPago";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            {/* Ruta para cargar pagina de inico en la ruta / */}
+            <Route path="/" element={<Home />} />
+
+            {/* Ruta de lo que va a ver el usuario final para pagar con Qr */}
+            <Route path="/user" element={<formQrPago/>} />
+
+          </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
