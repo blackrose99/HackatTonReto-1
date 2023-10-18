3d5001d85a8d1cb0efd281d6aaf2fa8a22431f47
luigistig <luigistig17@gmail.com>
2023-10-18 14:39:00 -0500

    E.2.2

--- src/UniversalCompontes/Footer.js
+++ src/UniversalCompontes/Footer.js
@@ -7,23 +7,32 @@ function Footer() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-3 datosContactos">
-          <i class='bx bx-user-circle'></i>
-          <div className="contact-info">
-            <p className="bold-text">CONTACT CENTER</p>
-            <p>01 8000 938 088</p>
-          </div>
-          <i class='bx bx-phone'></i>
-          <div className="contact-info">
-            <p className="bold-text">EN BUCARAMANGA</p>
-            <p>607 680 30 03</p>
-          </div>
+            <i class='bx bx-user-circle'></i>
+            <div className="contact-info">
+              <p className="bold-text">CONTACT CENTER</p>
+              <p>01 8000 938 088</p>
+            </div>
+            <i class='bx bx-phone'></i>
+            <div className="contact-info">
+              <p className="bold-text">EN BUCARAMANGA</p>
+              <p>607 680 30 03</p>
+            </div>
           </div>
           <div className="col-lg-3">
             <div className="footer-info">
-              <h2>Nuestra Cooperativa de Banco</h2>
-              <p>
-                Somos una entidad cooperativa comprometida con nuestros socios y la comunidad. Brindamos servicios financieros y soluciones bancarias que hacen la diferencia.
-              </p>
+              <h2>Puntos de Contacto</h2>
+              {/* Lista de enlaces */}
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#">Escribenos</a>
+                </li>
+                <li>
+                  <a href="#">Visitanos</a>
+                </li>
+                <li>
+                  <a href="#">Linea WhatsApp</a>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="col-lg-3">
