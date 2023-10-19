6709b9ada8aba990ef0bf5a1c27765afce7c283b
Luis Manuel Castaño <mannuelcast321@gmail.com>
2023-10-18 21:30:17 -0500

    v.41

--- src/EntityComponentes/Cliente/Cliente.js
+++ src/EntityComponentes/Cliente/Cliente.js
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import InfoClient from "./infoclient"; // Importa el componente InfoClient (asegúrate de que la mayúscula inicial esté correcta)
-
+import InfoClient from "./infoclient"; // Asegúrate de que la mayúscula inicial esté correcta
 import HeaderCliente from "./HeaderCliente";
+
 const ClientInfo = () => {
   const { id } = useParams();
   const [clientData, setClientData] = useState(null);
-  const [infoData, setInfoData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `http://192.168.4.15:5000/api/users/${id}`
-        );
+        const response = await fetch(`http://192.168.4.15:5000/api/users/${id}`);
         const data = await response.json();
         setClientData(data);
-        setCuentaData(data);
         setLoading(false);
       } catch (error) {
         console.error("Error al obtener datos del cliente", error);
@@ -36,14 +32,11 @@ const ClientInfo = () => {
     return <p>No se encontró información para este cliente.</p>;
   }
 
-  if (!cuentaData) {
-    return <p>No se encontró información para este cuenta.</p>;
-  }
   return (
-    <di>
+    <div> {/* Cambié "di" a "div" para corregir el error de sintaxis */}
       <HeaderCliente data={clientData} /> 
       <InfoClient data={clientData} /> {/* Pasa los datos a InfoClient */}
-    </di>
+    </div>
   );
 };
 
--- src/EntityComponentes/FormComponentes/FormTipoCuentaTipoForm.js
+++ src/EntityComponentes/FormComponentes/FormTipoCuentaTipoForm.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom'; // Import useHistory
-import HeaderCliente from "../../EntityComponentes/Cliente/HeaderCliente";
+import React, { useState, useEffect } from 'react';
+import Header from "../../UniversalCompontes/Header";
 import Footer from "../../UniversalCompontes/Footer";
 import './FormTipoCuentaTipoForm.css';
 
 const CombinedForm = () => {
-    const [selectedAccountType, setSelectedAccountType] = useState('');
+  const [accountTypes, setAccountTypes] = useState([]);
+  const [formTypes, setFormTypes] = useState([]);
+  const [selectedAccountType, setSelectedAccountType] = useState('');
   const [selectedFormType, setSelectedFormType] = useState('');
-  const history = useHistory(); // Get the history object
+  
+
+  // Simula obtener los tipos de cuentas desde la base de datos o una API.
+  // Reemplaza esto con tu lógica real para obtener los tipos de cuentas.
+  useEffect(() => {
+    const accountTypesData = [
+      { id: 1, name: 'Ahorro' },
+      { id: 2, name: 'Corriente' },
+      // Agregar más tipos de cuentas según sea necesario
+    ];
+
+    setAccountTypes(accountTypesData);
+  }, []);
+
+  // Simula obtener los tipos de formularios desde la base de datos o una API.
+  // Reemplaza esto con tu lógica real para obtener los tipos de formularios.
+  useEffect(() => {
+    const formTypesData = [
+      { id: 1, name: 'Inmobiliaria' },
+      { id: 2, name: 'Personal' },
+      // Agregar más tipos de formularios según sea necesario
+    ];
+
+    setFormTypes(formTypesData);
+  }, []);
 
   const handleAccountTypeChange = (type) => {
     setSelectedAccountType(type);
@@ -17,41 +42,45 @@ const CombinedForm = () => {
     setSelectedFormType(type);
   };
 
-  const handleNextClick = () => {
-    // Navigate to CustomForm and pass selected values as search params
-    history.push(`/plantillas?accountType=${selectedAccountType}&formType=${selectedFormType}`);
-  };
-
   return (
     <div>
-      <HeaderCliente />
-    <div className="container2">
-      <h2 className="combined-form-title">Seleccione el Tipo de Cuenta y el Tipo de Formulario</h2>
-      <div className="form-section col-lg">
+      <Header />
+    <div class="container2 ">
+      <h2 className="combined-form-title " >Seleccione el Tipo de Cuenta y el Tipo de Formulario</h2>
+      <div class="form-section col-lg">
         <label className="combined-form-label"><b>Selecciona el Tipo de Cuenta:</b></label>
-        <br />
+        <br></br>
         <select
           className="combined-form-select"
           value={selectedAccountType}
           onChange={(e) => handleAccountTypeChange(e.target.value)}
         >
           <option value="">Seleccione un tipo de cuenta</option>
-          {/* Account type options */}
+          {accountTypes.map((type) => (
+            <option key={type.id} value={type.id}>
+              {type.name}
+            </option>
+          ))}
         </select>
       </div>
-      <div className="form-section">
-        <label className="combined-form-label"><b>Selecciona el Tipo de Formulario:</b></label>
-        <br />
+      <div class="form-section">
+        <label className="combined-form-label"><b> Selecciona el Tipo de Formulario:</b></label>
+        <br></br>
         <select
           className="combined-form-select"
           value={selectedFormType}
           onChange={(e) => handleFormTypeChange(e.target.value)}
         >
           <option value="">Seleccione un tipo de formulario</option>
-          {/* Form type options */}
+          {formTypes.map((type) => (
+            <option key={type.id} value={type.id}>
+              {type.name}
+            </option>
+          ))}
         </select>
       </div>
-      <button className="combined-form-button" onClick={handleNextClick}>Siguiente</button>
+      {/* Resto del contenido del formulario */}
+      <button className="combined-form-button">Siguiente</button>
     </div>
     <Footer />
     </div>
